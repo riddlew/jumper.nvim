dcf2ed484d93d3a2a1982a39340e94590ede70b3
xorid <riddle.w@gmail.com>
2022-11-28 22:03:52 -0500

    Initial commit

---
+++ lua/jumper.lua
@@ -0,0 +1,166 @@
+local Path = require("plenary.path")
+
+local M = {}
+
+JumperConfig = {}
+JumperPaths = {}
+
+local defaults = {
+	save_file = "jumper.json",
+}
+
+local function tbl_has_entry(table, entry)
+	for _, v in pairs(table) do
+		if string.lower(v.path) == string.lower(entry) then
+			return true
+		end
+	end
+
+	return false
+end
+
+local function get_input_path(default)
+	local input_path = vim.fn.input("[Jumper] Path: ", default)
+
+	if input_path == "" then
+		return nil
+	end
+
+	-- strip trailing slashes and spaces
+	input_path = string.gsub(input_path, "%s*$", "")
+	input_path = string.gsub(input_path, "/*$", "")
+
+	return input_path
+end
+
+local function get_input_name(default, path)
+	local input_name = vim.fn.input({
+		prompt = "[Jumper] Name (blank to use path as name): ",
+		default = default,
+		cancelreturn = "::CANCEL::",
+	})
+
+	if input_name == "::CANCEL::" then
+		return nil
+	end
+
+	if input_name == "" then
+		return path
+	end
+
+	return input_name
+end
+
+function M.add()
+	local buf = vim.fn.expand("%:p")
+	local path = {}
+	local input_path = get_input_path(buf ~= "" and buf or vim.fn.getcwd())
+
+	if input_path == nil then
+		return
+	end
+
+	if tbl_has_entry(JumperPaths, input_path) then
+		vim.notify("[Jumper] This path already exists", vim.log.levels.WARN)
+		return
+	end
+
+	local input_name = get_input_name()
+
+	if input_name == nil then
+		return
+	end
+	if input_name == "" then
+		input_name = input_path
+	end
+
+	path.path = input_path
+	path.name = input_name
+
+	table.insert(JumperPaths, path)
+	M:save_paths()
+end
+
+function M.remove(path)
+	for i, v in pairs(JumperPaths) do
+		if v.path == path then
+			table.remove(JumperPaths, i)
+			return
+		end
+	end
+
+	vim.notify(
+		string.format("[Jumper] unable to remove path %s, doesn't exist!", path),
+		vim.log.levels.ERROR
+	)
+end
+
+function M.rename(path)
+	for _, v in pairs(JumperPaths) do
+		if v.path == path then
+			return
+		end
+	end
+
+	vim.notify(
+		string.format("[Jumper] unable to rename path %s, doesn't exist!", path),
+		vim.log.levels.ERROR
+	)
+end
+
+function M.edit(path)
+	for _, v in pairs(JumperPaths) do
+		if v.path == path.path then
+			local input_path = get_input_path(v.path)
+
+			if input_path == nil then
+				return
+			end
+
+			local input_name = get_input_name(v.name)
+
+			if input_name == nil then
+				return
+			end
+			if input_name == "" then
+				input_name = v.path
+			end
+
+			v.path = input_path
+			v.name = input_name
+			M:save_paths()
+			return
+		end
+	end
+
+	vim.notify(
+		string.format("[Jumper] unable to edit path %s, doesn't exist!", path),
+		vim.log.levels.ERROR
+	)
+end
+
+function M.save_paths()
+	JumperConfig.path:write(vim.json.encode(JumperPaths), "w", 420)
+end
+
+function M.load_paths()
+	return vim.json.decode(JumperConfig.path:read())
+end
+
+local function get_save_path()
+	return Path:new(vim.fn.stdpath("config") .. "/" .. JumperConfig.save_file)
+end
+
+function M.setup(opts)
+	opts = opts or {}
+	JumperConfig = vim.tbl_deep_extend("force", defaults, opts)
+	JumperConfig.path = get_save_path()
+
+	if not JumperConfig.path:exists() then
+		M.save_paths()
+	end
+
+	JumperPaths = M.load_paths()
+end
+
+return M
---
+++ lua/telescope/_extensions/jumper.lua
@@ -0,0 +1,170 @@
+local has_telescope, telescope = pcall(require, "telescope")
+local pickers = require("telescope.pickers")
+local conf = require("telescope.config").values
+local finders = require("telescope.finders")
+-- local entry_display = require("telescope.pickers.entry_display")
+local action_state = require("telescope.actions.state")
+local jumper = require("jumper")
+
+if not has_telescope then
+	error("This plugin requires nvim-telescope/telescope.nvim")
+end
+
+local function create_finder()
+	return finders.new_table({
+		results = JumperPaths,
+		entry_maker = function(entry)
+			-- local displayer = entry_display.create({
+			-- 	separator = " ",
+			-- 	items = {
+			-- 		{ width = 40 },
+			-- 		{ remaining = true },
+			-- 	}
+			-- })
+			local make_display = function()
+				-- return displayer({
+				-- 	entry.name,
+				-- 	{ string.format("(%s)", entry.path), "Comment" },
+				-- })
+				local line = entry.name .. " " .. entry.path
+				local highlights = {}
+				local hl_start = #entry.name + 1
+				local hl_end = hl_start + #entry.path
+				table.insert(highlights, { { hl_start, hl_end }, "Comment" })
+				return line, highlights
+			end
+			return {
+				value = entry,
+				ordinal = entry.path,
+				name = entry.name,
+				path = entry.path,
+				display = make_display,
+			}
+		end,
+	})
+end
+
+local function add_path(bufnr)
+	local current_picker = action_state.get_current_picker(bufnr)
+
+	jumper.add()
+	current_picker:refresh(create_finder(), { reset_prompt = false })
+end
+
+local function delete_path(bufnr)
+	local selected = action_state.get_selected_entry()
+	local should_delete = string.lower(
+		vim.fn.input(
+			string.format("[Jumper] delete %s? [y/n]: ", selected.name)
+		)
+	)
+	local current_picker = action_state.get_current_picker(bufnr)
+
+	if should_delete ~= "y" then
+		print("")
+		vim.notify("[Jumper] path delete cancelled", vim.log.levels.INFO)
+		return
+	end
+
+	jumper.remove(selected.path)
+	current_picker:refresh(create_finder(), { reset_prompt = false })
+end
+
+local function edit_path(bufnr)
+	local selected = action_state.get_selected_entry()
+	local current_picker = action_state.get_current_picker(bufnr)
+
+	jumper.edit(selected)
+	current_picker:refresh(create_finder(), { reset_prompt = false })
+end
+
+-- I'm using swap instead of move up / down because I haven't figured out
+-- a way to refresh the current picker without the selected line staying
+-- selected. Every time it refreshes, the selection goes to the top of the
+-- picker.
+local function swap_marked(bufnr)
+	local current_picker = action_state.get_current_picker(bufnr)
+	local selected = current_picker._multi:get()
+	if #selected < 2 then
+		vim.notify(
+			"[Jumper] need at least 2 selections to swap",
+			vim.log.levels.INFO
+		)
+		return
+	elseif #selected > 2 then
+		vim.notify(
+			"[Jumper] too many selections, only 2 can be swapped",
+			vim.log.levels.INFO
+		)
+		return
+	end
+
+	local i, j
+	if selected[1].index < selected[2].index then
+		i = selected[1]
+		j = selected[2]
+	else
+		i = selected[2]
+		j = selected[1]
+	end
+
+	table.remove(JumperPaths, i.index)
+	table.remove(JumperPaths, j.index - 1)
+	table.insert(JumperPaths, i.index, j.value)
+	table.insert(JumperPaths, j.index, i.value)
+	current_picker:refresh(create_finder(), { reset_prompt = true })
+end
+
+-- local function move_path_down(bufnr)
+-- 	local selection = action_state.get_selected_entry()
+-- 	local current_picker = action_state.get_current_picker(bufnr)
+--
+-- 	if selection.index == #JumperPaths then
+-- 		return
+-- 	end
+--
+-- 	table.remove(JumperPaths, selection.index)
+-- 	table.insert(JumperPaths, selection.index + 1, selection.value)
+-- 	current_picker:refresh(create_finder(), { reset_prompt = true })
+-- end
+--
+-- local function move_path_up(bufnr)
+-- 	local selection = action_state.get_selected_entry()
+-- 	local current_picker = action_state.get_current_picker(bufnr)
+--
+-- 	if selection.index == 1 then
+-- 		return
+-- 	end
+--
+-- 	table.remove(JumperPaths, selection.index)
+-- 	table.insert(JumperPaths, selection.index - 1, selection.value)
+-- 	current_picker:refresh(create_finder(), { reset_prompt = true })
+-- end
+
+local function picker(opts)
+	opts = opts or {}
+	pickers
+		.new(opts, {
+			prompt_title = "Jumper",
+			finder = create_finder(),
+			sorter = conf.generic_sorter(opts),
+			previewer = conf.grep_previewer(opts),
+			attach_mappings = function(_, map)
+				map("n", "<C-d>", delete_path)
+				map("i", "<C-d>", delete_path)
+
+				map("n", "<C-a>", add_path)
+				map("i", "<C-a>", add_path)
+
+				map("n", "<C-e>", edit_path)
+				map("i", "<C-e>", edit_path)
+
+				map("n", "<C-s>", swap_marked)
+				map("i", "<C-s>", swap_marked)
+				return true
+			end,
+		})
+		:find()
+end
+
+return telescope.register_extension({ exports = { jumper = picker } })
